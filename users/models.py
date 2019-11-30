731a7f5e59015d8160ba326c220e7fd3643ce423
yamadaitsuki <yamada351035@gmail.com>
2019-11-30 15:53:46 +0900

    ADD:models.UserManager
    - Userモデルを作成するためのメソッドを管理する。これは、Userモデルの中のobjectsという変数で管理している。
    - user_in_migrations = Trueとすることでこれらのクラスをマイグレーションで管理できるようにする

--- users/models.py
+++ users/models.py
@@ -1,10 +1,40 @@
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 from django.utils import timezone
 
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    """カスタムユーザマネージャー"""
+    user_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        # emailを必須にする
+        if not email:
+            raise ValueError('The given email must be set')
+        # emailでUserモデルを作成
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email=email, password=password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('SuperUser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        return self._create_user(email=email, password=password, **extra_fields)
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     """カスタムユーザモデル"""
     initial_point = 50000
