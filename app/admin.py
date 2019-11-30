8c81e3f3a75fa4f2ae31302c89babf4545a293bd
yamadaitsuki <yamada351035@gmail.com>
2019-11-30 17:51:25 +0900

    ADD:Product,Saleモデル
    - adminへの登録時はadmin.site.register(モデル名)で追加可能
    - ImageFieldを使用するときはPillowライブラリを忘れずにインポートする必要がある

--- app/admin.py
+++ app/admin.py
@@ -1,1 +1,7 @@
 # Register your models here.
+from django.contrib import admin
+
+from app.models import Product, Sale
+
+admin.site.register(Product)
+admin.site.register(Sale)
--- app/models.py
+++ app/models.py
@@ -1,1 +1,23 @@
 # Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+class Product(models.Model):
+    """商品"""
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='product')
+
+    def __str__(self):
+        return self.name
+
+
+class Sale(models.Model):
+    """売上情報"""
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    users = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+    amount = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField("商品単価")
+    created_at = models.DateTimeField(auto_now=True)
