949592a5e58b259ab67f2d7bc35004ad3bfc66b7
yamadaitsuki <yamada351035@gmail.com>
2019-11-30 17:03:43 +0900

    ADD:Login Logout機能を実装
    - login.htmlのログインフォームでemailではなく、usernameとしているのはカスタムユーザモデル作成時にUSERNAME_FIELDをemailと指定したため
    - login時はurlconfにLoginView
    - logout時はurlconfにLogoutView

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,5 @@ app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
